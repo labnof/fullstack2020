aaf922f3a8ff568c1c7979995d13b8ec1f06ce54
Babatund Anafi <labnof@gmail.com>
2020-08-20 13:47:04 +0300

    all lates

--- class/part3/server/index.js
+++ class/part3/server/index.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 let notes = [
   {
@@ -22,6 +36,8 @@ let notes = [
     important: true
   }
 ]
+
+app.use(requestLogger)
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -74,7 +90,9 @@ app.post('/api/notes', (request, response) => {
 
   response.json(note)
 })
-const PORT = 3001
+
+app.use(unknownEndpoint)
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
